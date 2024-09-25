7311a70a7a8b3f4c28293a3be27e75d3ac055587
Shashi Singh <shashisingh25494@gmail.com>
2024-09-25 15:31:52 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,15 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 };
 
 
