585504df8e61ab203bd716c79391528988246664
Shashi Singh <shashisingh25494@gmail.com>
2024-09-25 15:27:59 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let st = [];
+
+        for (let i = 0; i < s.length; i++) {
+            let ch = s.charAt(i);
+            if (st.length === 0 && (ch === '}' || ch === ']' || ch === ')')) {
+                return false;
+            } else {
+                if (ch === '(' || ch === '{' || ch === '[') {
+                    st.push(ch);
+                } else if (ch === ')' && st[st.length - 1] === '(') {
+                    st.pop();
+                } else if (ch === '}' && st[st.length - 1] === '{') {
+                    st.pop();
+                } else if (ch === ']' && st[st.length - 1] === '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.length === 0;
 };
 
 module.exports = { isValid };
